0215278937cdb300cffd0946a5d5127f355562f7
Taisiya <kurmaevataisija@mail.ru>
2014-02-16 10:55:41 +0200

    commit 2

--- js/notebook.js
+++ js/notebook.js
@@ -1,6 +1,45 @@
+var DROPBOX_APP_KEY = 'wy9kcn6i5kkgrrf';
+var client = new Dropbox.Client({key: DROPBOX_APP_KEY});
 $(function(){
+// The login button will start the authentication process.
+	$('#loginButton').click(function (e) {
+		e.preventDefault();
+		// This will redirect the browser to OAuth login.
+		client.authenticate();
+	});
 
-var Note=Backbone.Model.extend({
+	// Try to finish OAuth authorization.//false 
+	client.authenticate({interactive:false}, function (error) {
+		if (error) {
+			alert('Authentication error: ' + error);
+		}
+		else alert('ok');
+	});
+
+	if (client.isAuthenticated()) {
+		// Client is authenticated. Display UI.
+		$('#loginButton').hide();
+		$('#m').show();
+
+		client.getDatastoreManager().openDefaultDatastore(function (error, datastore) {
+			if (error) {
+				alert('Error opening default datastore: ' + error);
+			}
+			else alert('It is all right');
+
+		/*	taskTable = datastore.getTable('tasks');
+
+			// Populate the initial task list.
+			updateList();
+
+			// Ensure that future changes update the list.
+			datastore.recordsChanged.addListener(updateList);*/
+		});
+	}
+	if(!client.isAuthenticated()) {alert('not ok ');}
+
+
+/*var Note=Backbone.Model.extend({
 EMPTY:
 {
   text:'none',
@@ -10,6 +49,6 @@ initialize: function(){
 if(!this.get("content")){this.set({"content": this.EMPTY});}
 },//init
 
-});//model
+});//model*/
 
 });//$(function()
