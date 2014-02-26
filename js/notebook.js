cdcba042e32999473e5dcc0f0c5890c565211c48
Taisiya <kurmaevataisija@mail.ru>
2014-02-26 14:27:10 +0200

    commit 5

--- js/notebook.js
+++ js/notebook.js
@@ -1,8 +1,10 @@
 var DROPBOX_APP_KEY = 'wy9kcn6i5kkgrrf';
 var client = new Dropbox.Client({key: DROPBOX_APP_KEY});
 var Table;
-var idn, ident;
-$(function(){
+var idn;
+var isNewNote;
+$(function()
+{
 
     function show_edit_page()
 	{
@@ -15,224 +17,234 @@ $(function(){
 	     $('#for_edit').hide();
 	}
 	
-// The login button will start the authentication process.
-	$('#loginButton').click(function (e) {
+	$('#loginButton').click(function (e) 
+	{
 		e.preventDefault();
-		// This will redirect the browser to OAuth login.
 		client.authenticate();
 	});
 
-	// Try to finish OAuth authorization.
-	client.authenticate({interactive:false}, function (error) {
-		if (error) {
+	client.authenticate({interactive:false}, function (error)
+	{
+		if (error) 
+		{
 			alert('Authentication error: ' + error);
 		}
 	});
 
-	if (client.isAuthenticated()) {
-		// Client is authenticated. Display UI.
+	if (client.isAuthenticated()) 
+	{
 		$('#loginButton').hide();
 		$('#edit_button').hide();
-
-		client.getDatastoreManager().openDefaultDatastore(function (error, datastore) {
-			if (error) {
+		client.getDatastoreManager().openDefaultDatastore(function (error, datastore)
+		{
+			if (error) 
+			{
 				alert('Error opening default datastore: ' + error);
 			}
-		
 			Table = datastore.getTable('Notes');
-			// Populate the initial task list.
 			updateList();
-			// Ensure that future changes update the list.
 			datastore.recordsChanged.addListener(updateList);
 		});
 	}
-	function insertNote(note_name, text) {
-		Table.insert({
-			name: note_name,
+	
+	function insertNote(text) 
+	{
+		Table.insert(
+		{
 			content: text,
-			modified: new Date() });
+			modified: getTime_and_Date()
+		});
 	}
 	
-	function updateList() {
+	function updateList() 
+	{
 		$('#notes_list').empty();
-
 		var records = Table.query();
-
 		// Sort by creation time.
-		records.sort(function (note_A, note_B) {
+		records.sort(function (note_A, note_B) 
+		{
 			if (note_A.get('modified') < note_B.get('modified')) return -1;
 			if (note_A.get('modified') > note_B.get('modified')) return 1;
 			return 0;
 		});
-		
 		if(records.length>0)
 		{
-         $('#em').hide();
-		for (var i = 0; i < records.length; i++) {
-			var record = records[i];
-			$('#notes_list').append(
-				renderNote(record.getId(),
-					record.get('name')));
+			$('#em').hide();
+			// Add an item to the list for each note.
+			for (var i = 0; i < records.length; i++) 
+			{
+				var record = records[i];
+				$('#notes_list').append(renderNote(record.getId(),record.get('content')));//name
+			}
 		}
+		else 
+		{
+			$('#em').show();
+			$('#edit_button').hide();
+			$('#for_note1').empty();
+			$('#l1').empty();
 		}
-		else $('#em').show();
         $('#n').hide();
 		addListeners();
 		$('#newNote').focus();
 	}
 	
-	function renderNote(id, name) {
-	    var n = '&nbsp &nbsp'+name;
+	function getNameString(content)
+	{
+		var n ='';
+		if (content.length < 25) n = content;
+		else for (var i = 0; i < 26; i++) n = n + content[i];
+		return n;
+	}
+	
+	function renderNote(id, content)
+	{
 		return $('<li>').attr('id', id).append(
-				$('<input>').attr('type', 'checkbox')
-			).append(
-				$('<a>').addClass('cl').html(n));
-
+				$('<input>').attr('type', 'checkbox').attr('checked', false)).append(
+				$('<a>').addClass('cl').text(getNameString(content)));
 	}
 	
 	function show_note(id)
 	{ 
-	   $('#for_note1').empty();
-	   $('#l1').empty();
-	   $('#edit_button').show();
-	   
-	   var records = Table.query();
-	   for (var i = 0; i < records.length; i++)
-	   {
-	      var record = records[i];
-		  if(record.getId()==id) 
-	   {
-	   $('#for_note1').append(record.get('content'));
-	   $('#l1').append(record.get('name'));
-	   
-	   break;
-	   }
-	   }
+		$('#for_note1').empty();
+		$('#l1').empty();
+		$('#edit_button').show();
+		var records = Table.query();
+		for (var i = 0; i < records.length; i++)
+		{
+			var record = records[i];
+			if(record.getId()==id) 
+			{
+			    var n = record.get('content');
+				$('#for_note1').text(n);
+				$('#l1').text(getNameString(n));
+				break;
+			}
+		}
 	}
 	
-	function deleteRecord(id) {
+	function deleteRecord(id) 
+	{
 		Table.get(id).deleteRecord();
 	}
 
-	$('#add_note').click(function(e){
-	    // e.preventDefault();
-		 ident=0;
-	     $('#edit_time').empty();
-		 $('#newNote').val('');
-		 $('#newText').val('');
-		 show_edit_page();
+	$('#add_note').click(function(e)
+	{
+		isNewNote=true;
+	    $('#edit_time').empty();
+		$('#newNote').val('');
+		$('#newText').val('');
+		show_edit_page();
 	});
 	
-	$('#edit_button').click(function(e){
-	//	e.preventDefault();
+	$('#edit_button').click(function(e)
+	{
 		edit_note(idn);
 		show_edit_page();
-		ident=1;
 	});
 	
-	$('#save').click(function(e){
-		//e.preventDefault();
-		if(ident==1) change_note(idn);
-		else add_new_note();});
-		
-	$('#exit').click(function(e){
-	    // e.preventDefault();
-		 show_main_page();
-		 if(ident==1) 
+	$('#save').click(function(e)
+	{
+		if(!isNewNote) 
 		{
-		show_note(idn);
-		ident=0;
-		}
+			change_note(idn);
+			show_note(idn);
+		}	
+		else add_new_note();
+		show_main_page();
+		isNewNote=false;
+	});
+		
+	$('#exit').click(function(e)
+	{
+		show_main_page();
+		if(!isNewNote)
+			show_note(idn);
     });	
 	
-	$('#del_note').click(function (e){
-	});
-	
-	function addListeners() {
-
-		$('button.delete').click(function (e) {
-			//e.preventDefault();
-			var id = $(this).parents('li').attr('id');
-			deleteRecord(id);
+	$('#del_note').click(function (e)
+	{ 
+		var id_note;
+		$('input').each(function()
+		{ 
+			id_note=$(this).parents('li').attr('id');
+			if($(this).attr('checked')==true)
+			{
+				deleteRecord(id_note);
+			}
 		});
-		
-	$('a.cl').click(function(e){
-		//e.preventDefault();
-		show_main_page();
-	    idn=$(this).parents('li').attr('id');
-		show_note(idn);
 	});
 	
+	function addListeners() 
+	{
+		$('a.cl').click(function(e)
+		{
+			show_main_page();
+			idn=$(this).parents('li').attr('id');
+			show_note(idn);
+		});
+	
+		$('li').click(function(e)
+		{
+			show_main_page();
+			idn=$(this).attr('id');
+			show_note(idn);
+		});	
 	} 
 	
 	function change_note(id)
 	{ 
-
-	    if ($('#newNote').val().length > 0) {
-		    if($('#newText').val().length>0)
-			{
-			var v1, v2, v3;
-			v1=$('#newNote').val();
-			v2=$('#newText').val();
-			v3=new Date();
-			Table.get(id).set('name', v1);
-			Table.get(id).set('content', v2);
-			Table.get(id).set('modified', v3);
-			}
-			else alert('text not found. change');
+		if($('#newText').val().length>0)
+		{
+			Table.get(id).set('content', $('#newText').val());
+			Table.get(id).set('modified',  getTime_and_Date());
 		}
-		else alert('name not found. change');
-  	return false;
+		else alert('text not found.');
+		return false;
 	}
 	
 	function edit_note(id)
 	{
-	   $('#newText').val('');
-	   $('#newNote').val('');
-	   $('#edit_time').empty();
-	   var records = Table.query();
-	   for (var i = 0; i < records.length; i++)
-	   {
-	      var record = records[i];
-		  if(record.getId()==id) 
-	   {
-	   var v11, v21, v31;
-	   v11=record.get('content');
-	   v21=record.get('name');
-	   v31=record.get('modified').toString();
-	   $('#newText').val(v11);
-	   $('#newNote').val(v21);
-	   $('#edit_time').append(v31);
-	   break;
-	   }
-	   }
+		$('#newText').val('');
+		$('#edit_time').empty();
+		var records = Table.query();
+		for (var i = 0; i < records.length; i++)
+		{
+			var record = records[i];
+			if(record.getId()==id) 
+			{
+				$('#newText').val(record.get('content'));
+				$('#edit_time').append((record.get('modified')));
+				break;
+			}
+		}
 	}
 	
 	function add_new_note()
 	{
-	   if ($('#newNote').val().length > 0) {
-		    if($('#newText').val().length>0)
-			{
-			insertNote($('#newNote').val(), $('#newText').val());
-			$('#newNote').val('');
+		if($('#newText').val().length>0)
+		{
+			insertNote( $('#newText').val());
 			$('#newText').val('');
-			}
-			else alert('text not found. add');
 		}
-		else alert('name not found. add');
+		else alert('text not found.');
 		return false;
 	}
 	
-/*var Note=Backbone.Model.extend({
-EMPTY:
-{
-  text:'none',
-  last_modified:'none',
-}
-initialize: function(){
-if(!this.get("content")){this.set({"content": this.EMPTY});}
-},//init
-
-});//model*/
-
+	function getTime_and_Date()
+	{
+		var td = new Date();
+		var hh = td.getHours();
+		if (hh<10) hh = '0'+hh;
+		var mm = td.getMinutes();
+		if(mm<10) mm = '0'+mm;
+		var day = td.getDate();
+		if (day<10) day = '0'+day;
+		var month = td.getMonth()+1;
+		if (month<10) month = '0'+month;
+		var year = td.getFullYear();
+		var str = hh + ":" + mm +", " + day + "-" + month + "-" + year;
+		return str;
+	}
+	
 });//$(function()
